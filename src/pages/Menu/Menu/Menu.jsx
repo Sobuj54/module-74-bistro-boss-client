b8de1ffb7efb05bcba400020bdd48fbeaef8fc78
soubj <sobujahmed124@gmail.com>
2023-08-13 01:25:23 +0600

    showing todays offer using a component

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,14 +1,29 @@
 import Cover from "../../shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
 import { Helmet } from "react-helmet-async";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/Sectiontitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+  const [menu] = useMenu();
+
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="Our Menu"></Cover>
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's Offer"></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
     </div>
   );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,5 +1,20 @@
-const MenuCategory = () => {
-  return <div></div>;
+import MenuItem from "../../shared/MenuItem/MenuItem";
+
+const MenuCategory = ({ items }) => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-2 gap-12 mx-4 md:mx-0">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="text-center mt-7">
+        <button className="btn btn-outline border-0 border-b-4 mt-3 ">
+          View Full Menu
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
