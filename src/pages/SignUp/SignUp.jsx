79da5401b690a5a4facc86b35cd28b3b823a39c8
soubj <sobujahmed124@gmail.com>
2023-08-14 21:25:28 +0600

    used react hook form in singup form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,71 +1,104 @@
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row">
-        <div className="text-center w-1/2 lg:text-left">
-          <h1 className="text-5xl font-bold">SignUp now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card  w-full max-w-md shadow-2xl bg-base-100">
-          <form className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                name="name"
-                required
-                placeholder="Name"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                required
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                name="password"
-                required
-                placeholder="password"
-                className="input input-bordered"
-              />
-            </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-            <div className="form-control mt-6">
-              <input type="submit" value="Login" className="btn btn-primary" />
-            </div>
-          </form>
-          <p className="text-center py-5">
-            <small>
-              Already Have an Account?
-              <Link to="/login" className="text-orange-500 ml-2">
-                Login
-              </Link>
-            </small>
-          </p>
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <Helmet>
+        <title>Bistro Boss | SignUp</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="text-center w-1/2 lg:text-left">
+            <h1 className="text-5xl font-bold">SignUp now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card  w-full max-w-md shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name")}
+                  name="name"
+                  required
+                  placeholder="Name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  {...register("email")}
+                  required
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  {...register("password", {
+                    pattern:
+                      /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}/,
+                  })}
+                  required
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                {/* showing error depending on the user password input */}
+                {errors.password?.type === "pattern" && (
+                  <p>
+                    Minimum 8 characters Maximum 20 characters At least one
+                    uppercase character At least one lowercase character At
+                    least one digit At least one special character{" "}
+                  </p>
+                )}
+              </div>
+
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
+            <p className="text-center py-5">
+              <small>
+                Already Have an Account?
+                <Link to="/login" className="text-orange-500 ml-2">
+                  Login
+                </Link>
+              </small>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
