c15360577f65b6d6e339116ef0d86a91a9d4743c
soubj <sobujahmed124@gmail.com>
2023-08-19 17:19:40 +0600

    created user cart table and loaded data

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyCart = () => {
   const [cart] = useCart();
@@ -7,15 +8,54 @@ const MyCart = () => {
   //   js reduce function is used to get sum of all the values
   const total = cart.reduce((sum, item) => sum + item.price, 0);
   return (
-    <>
+    <div className="w-full">
       <Helmet>
         <title>Bistro Boss | My Cart</title>
       </Helmet>
-      <div>
+      <div className="flex justify-evenly font-semibold">
         <h3 className="text-3xl">Total Items : {cart.length}</h3>
-        <h3 className="text-3xl">Total Price : $ {total}</h3>
+        <h3 className="text-3xl">Total Price : ${total}</h3>
+        <button className="btn btn-sm btn-warning">Pay</button>
       </div>
-    </>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Food</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={item.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td className="text-end">${item.price}</td>
+                <th>
+                  <button className="btn bg-red-500 text-white btn-lg">
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
