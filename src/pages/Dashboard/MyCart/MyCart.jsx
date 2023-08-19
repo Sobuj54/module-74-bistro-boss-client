584624ef61c30c81b3ffb4aed9b938bde042f755
soubj <sobujahmed124@gmail.com>
2023-08-19 15:23:50 +0600

    js reduce method to calculate sum in array of objects

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,13 +1,21 @@
 import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
 
 const MyCart = () => {
+  const [cart] = useCart();
+
+  //   js reduce function is used to get sum of all the values
+  const total = cart.reduce((sum, item) => sum + item.price, 0);
   return (
-    <div>
+    <>
       <Helmet>
         <title>Bistro Boss | My Cart</title>
       </Helmet>
-      This is dashboard.
-    </div>
+      <div>
+        <h3 className="text-3xl">Total Items : {cart.length}</h3>
+        <h3 className="text-3xl">Total Price : $ {total}</h3>
+      </div>
+    </>
   );
 };
 
