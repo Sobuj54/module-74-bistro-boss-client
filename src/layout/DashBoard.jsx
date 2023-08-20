8de98506545a15491cdd2bc152f57b381bdd5025
soubj <sobujahmed124@gmail.com>
2023-08-20 18:49:30 +0600

    created admin dashboard

--- src/layout/DashBoard.jsx
+++ src/layout/DashBoard.jsx
@@ -5,12 +5,17 @@ import {
   FaWallet,
   FaHamburger,
   FaFirstOrderAlt,
+  FaUtensils,
+  FaBook,
+  FaUsers,
 } from "react-icons/fa";
 import { Link, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
 const DashBoard = () => {
   const [cart] = useCart();
+
+  const isAdmin = true;
 
   return (
     <div className="drawer lg:drawer-open ">
@@ -28,29 +33,62 @@ const DashBoard = () => {
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 h-full">
           {/* Sidebar content here */}
-          <li>
-            <Link to="/dashboard/home">
-              <FaHome /> User Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/reservations">
-              <FaCalendar /> Reservations
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/mycart">
-              <FaShoppingCart /> My Cart{" "}
-              <span className="badge badge-secondary">
-                +{cart?.length || 0}
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/history">
-              <FaWallet></FaWallet> Payment History
-            </Link>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <Link to="/dashboard/home">
+                  <FaHome /> Admin Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard/reservations">
+                  <FaUtensils /> Add Item
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/dashboard/history">
+                  <FaWallet></FaWallet> Manage Items
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard/history">
+                  <FaBook></FaBook> Manage Bookings
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard/allusers">
+                  <FaUsers></FaUsers> All Users
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/dashboard/home">
+                  <FaHome /> User Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard/reservations">
+                  <FaCalendar /> Reservations
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard/mycart">
+                  <FaShoppingCart /> My Cart
+                  <span className="badge badge-secondary">
+                    +{cart?.length || 0}
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard/history">
+                  <FaWallet></FaWallet> Payment History
+                </Link>
+              </li>
+            </>
+          )}
           <div className="divider"></div>
           <li>
             <Link to="/">
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,6 +8,7 @@ import SignUp from "../pages/SignUp/SignUp";
 import DashBoard from "../layout/DashBoard";
 import MyCart from "../pages/Dashboard/MyCart/MyCart";
 import PrivateRoute from "./PrivateRoute";
+import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
 
 export const router = createBrowserRouter([
   {
@@ -47,6 +48,10 @@ export const router = createBrowserRouter([
       {
         path: "mycart",
         element: <MyCart></MyCart>,
+      },
+      {
+        path: "allusers",
+        element: <AllUsers></AllUsers>,
       },
     ],
   },
