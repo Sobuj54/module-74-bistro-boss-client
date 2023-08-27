b272c11badb7c222423e06be2adcbddf52b5c42e
soubj <sobujahmed124@gmail.com>
2023-08-28 00:29:05 +0600

    successfully added newItem and react toast

--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -1,10 +1,14 @@
 import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../components/Sectiontitle/SectionTitle";
 import { useForm } from "react-hook-form";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const imageHostingToken = import.meta.env.VITE_image_upload_token;
 
 const AddItem = () => {
+  const [axiosSecure] = useAxiosSecure();
   const { register, handleSubmit } = useForm();
   // image hosting on imgbb site
   const imageHostingURL = `https://api.imgbb.com/1/upload?key=${imageHostingToken}`;
@@ -32,88 +36,99 @@ const AddItem = () => {
             recipe,
             image: imgURL,
           };
-          console.log(newItem);
+
+          // posting using axios
+          axiosSecure.post("/menu", newItem).then((data) => {
+            console.log("data after posting : ", data.data);
+            if (data.data.insertedId) {
+              toast.success("You've successfully added an item !", {
+                position: toast.POSITION.TOP_CENTER,
+              });
+            }
+          });
         }
       });
   };
 
-  console.log(imageHostingToken);
   return (
-    <div className="w-full px-10">
-      <Helmet>
-        <title>Bistro Boss | Add An Item</title>
-      </Helmet>
-      <SectionTitle
-        subHeading="What's New"
-        heading="Add An Item"></SectionTitle>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-control w-full ">
-          <label className="label">
-            <span className="label-text">Recipe Name*</span>
-          </label>
-          <input
-            type="text"
-            {...register("name", { required: true })}
-            placeholder="Recipe Name"
-            className="input input-bordered w-full "
-          />
-        </div>
-        <div className="flex gap-5 my-4">
+    <>
+      <div className="w-full px-10">
+        <Helmet>
+          <title>Bistro Boss | Add An Item</title>
+        </Helmet>
+        <SectionTitle
+          subHeading="What's New"
+          heading="Add An Item"></SectionTitle>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control w-full ">
             <label className="label">
-              <span className="label-text">Category*</span>
-            </label>
-            <select
-              defaultValue="Pick One"
-              {...register("category", { required: true })}
-              className="select select-bordered">
-              <option disabled>Pick One</option>
-              <option>Pizza</option>
-              <option>Soup</option>
-              <option>Salad</option>
-              <option>Drinks</option>
-              <option>Dessert</option>
-            </select>
-          </div>
-          <div className="form-control w-full ">
-            <label className="label">
-              <span className="label-text">Price*</span>
+              <span className="label-text">Recipe Name*</span>
             </label>
             <input
-              type="number"
-              {...register("price", { required: true })}
-              placeholder="Price"
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="Recipe Name"
               className="input input-bordered w-full "
             />
           </div>
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Recipe Details*</span>
-          </label>
-          <textarea
-            {...register("recipe", { required: true })}
-            className="textarea textarea-bordered h-24"
-            placeholder="Recipe Details"></textarea>
-        </div>
-        {/* file input */}
-        <div className="form-control w-full my-4">
-          <label className="label">
-            <span className="label-text">Item Image*</span>
-          </label>
+          <div className="flex gap-5 my-4">
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text">Category*</span>
+              </label>
+              <select
+                defaultValue="Pick One"
+                {...register("category", { required: true })}
+                className="select select-bordered">
+                <option disabled>Pick One</option>
+                <option>Pizza</option>
+                <option>Soup</option>
+                <option>Salad</option>
+                <option>Drinks</option>
+                <option>Dessert</option>
+              </select>
+            </div>
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text">Price*</span>
+              </label>
+              <input
+                type="number"
+                {...register("price", { required: true })}
+                placeholder="Price"
+                className="input input-bordered w-full "
+              />
+            </div>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Recipe Details*</span>
+            </label>
+            <textarea
+              {...register("recipe", { required: true })}
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe Details"></textarea>
+          </div>
+          {/* file input */}
+          <div className="form-control w-full my-4">
+            <label className="label">
+              <span className="label-text">Item Image*</span>
+            </label>
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input file-input-bordered w-full "
+            />
+          </div>
           <input
-            {...register("image", { required: true })}
-            type="file"
-            className="file-input file-input-bordered w-full "
+            type="submit"
+            value="Add Item"
+            className="btn btn-md btn-secondary my-4"
           />
-        </div>
-        <input
-          type="submit"
-          value="Add Item"
-          className="btn btn-md btn-secondary my-4"
-        />
-      </form>
-    </div>
+        </form>
+      </div>
+      <ToastContainer />
+    </>
   );
 };
 
