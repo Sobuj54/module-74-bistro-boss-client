ee4e968d44179f44c04a3160d24e7300ec45d7b9
soubj <sobujahmed124@gmail.com>
2023-08-28 23:54:21 +0600

    created checkout credit card form using stripe

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,17 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/Sectiontitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(import.meta.env.VITE_payment_gateway_PK);
 
 const Payment = () => {
   return (
     <div className="w-full">
       <SectionTitle heading="Payment" subHeading="Process"></SectionTitle>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
     </div>
   );
 };
