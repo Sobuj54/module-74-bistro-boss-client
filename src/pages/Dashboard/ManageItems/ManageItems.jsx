3b4d4155a9d2374b9e7dd137e1c41992c75f703c
soubj <sobujahmed124@gmail.com>
2023-08-28 18:55:19 +0600

    showed all item in tabular form

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -13,6 +13,53 @@ const ManageItems = () => {
       <SectionTitle
         heading="Manage All Items"
         subHeading="Hurry Up"></SectionTitle>
+      {/* All items */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {menu.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{item.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>Zemlak, Daniel and Leannon</td>
+                <td>Purple</td>
+                <td>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </td>
+                <td>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
