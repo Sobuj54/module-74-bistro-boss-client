875c293570fa5c4d8793c8ad8e553140f0a75848
soubj <sobujahmed124@gmail.com>
2023-08-29 18:57:45 +0600

    connected to stripe api and showed card errors

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,8 +1,10 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [cardError, setCardError] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -15,30 +17,49 @@ const CheckoutForm = () => {
     if (card == null) {
       return;
     }
+
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("error : ", error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+      console.log("Payment Method : ", paymentMethod);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form onSubmit={handleSubmit} className="w-1/2 mx-auto my-10 space-y-7">
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button className="btn btn-accent" type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+      {cardError && (
+        <p className="text-red-500 w-1/2 mx-auto my-5">{cardError}</p>
+      )}
+    </>
   );
 };
 
